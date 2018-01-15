702b3fdfa1bee090d66553f9ab4fff36500a45b1
Bob van de Wiel <Bwiel1@avans.nl>
2018-01-15 20:16:18 +0100

    Login GUI gemaakt, nog geen actionListeners

--- src/GuiFixed/Gui.java
+++ src/GuiFixed/Gui.java
@@ -1,39 +1,47 @@
 package GuiFixed;
 
-import oracle.jrockit.jfr.JFR;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Bob on 15-1-2018.
  */
 public class Gui implements Runnable {
-    private JPanel panelMain;
+    private JPanel panelLogin;
     private JLabel nameFooter;
     private JLabel namePeople;
     private JPanel Footer;
     private JPanel buttonTabs;
-    private JButton overzicht1Button;
+    private JButton login;
     private JButton overzicht2Button;
     private JButton overzicht3Button;
     private JButton overzicht4Button;
+    private JFrame frame;
     private JPanel centerMain;
     private JComboBox comboBox1;
-    private JPanel selectSerie;
     private JPanel resultSelectSerie;
-    private JFrame frame;
+    private JPanel selectSerie;
+
 
     public Gui(){
 
+
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setContentPane(new Gui().frame);
+            }
+        });
     }
+
 
     @Override
     public void run() {
         this.frame = new JFrame("Netflix Statistix");
         this.frame.setPreferredSize(new Dimension(700, 700));
-        this.frame.setContentPane(new Gui().panelMain);
-
+        this.frame.setContentPane(new Gui().panelLogin);
         this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 
@@ -46,6 +54,8 @@ public class Gui implements Runnable {
 
     public JFrame getFrame() {
         return this.frame;
-    }
-}
+    }}
+
+
+
 
